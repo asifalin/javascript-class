34eb0c9e651cf407164b42c3afdd7235819e89d3
asifali12345678 <asifnagri55@gmail.com>
2024-02-12 17:02:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// var name= 'nsf';
+// console.log(name)
+// let name1='ali'
+// console.log(name1)
+// console.log('my company name is',name ,'my name is',name1)
+// document.write('my company name is',name ,'my name is',name1)
+
+// console.log('hellow')
+// alert ('hellow')
+// document.write("hello javascript")
+// var name ='hello javascript'
+// name ='hellow iam ali'
+// console.log(name)
+// let name1 ="helo javascript"
+// name1 = 'hellow js'
+// console.log('this is let varible',name1)
+const name='this is constant value'
+name1='hello name 1'
+console.log('hello this is constant varible',name ,'this is name1', name1)
+
+// Data type 
+// numbar
+let num1 =22;
+let num2 =32;
+console.log(num1,num2)
+// Boolean
+let num3 ='false'
+console.log(num3)
+// object
+let obj ={
+    name :'ali',
+    fname :'abbas'
+}
+console.log(obj)
+
+let array =['ali','abbas'
+]
+console.log(array)
